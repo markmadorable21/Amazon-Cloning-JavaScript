3261cd7a92df82247b4d57e5607c8bff0e328adc
MarkJudayaDev <markazjudaya@gmail.com>
2025-08-09 14:43:29 +0800

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,17 @@
-export const cart = [];
 // export means to outside, so other classes or outside this file can access this cart variable
+export const cart = [
+  {
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },
+
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 5,
+  },
+];
+// NORMALIZING THE DATA OR DE-DUPLICATING THE DATA
+//not including the other product details, the important is the productId so that we can reuse the product.js data
 
 export function addToCart(productId) {
   let matchingItem;

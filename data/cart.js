6471ee587edb12d352d894b652277c8b66f0b06f
MarkJudayaDev <markazjudaya@gmail.com>
2025-08-14 15:03:55 +0800

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -12,7 +12,7 @@ if (!cart) {
     {
       productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
       quantity: 2,
-      deliveryOptionId: '2',
+      deliveryOptionId: '3',
     },
   ];
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -34,10 +34,25 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
+
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  const today = dayjs();
+  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+  const dateString = deliveryDate.format('dddd, MMMM D');
+
   cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${
     matchingProduct.id
   }">
-      <div class="delivery-date">Delivery date: Tuesday, June 21</div>
+      <div class="delivery-date">Delivery date:${dateString}</div>
 
       <div class="cart-item-details-grid">
         <img
@@ -81,7 +96,7 @@ cart.forEach((cartItem) => {
             Choose a delivery option:
           </div>
           
-         ${deliveryOptionsHTML(matchingProduct)}
+         ${deliveryOptionsHTML(matchingProduct, cartItem)}
          
           
         </div>
@@ -89,7 +104,7 @@ cart.forEach((cartItem) => {
     </div>`;
 });
 
-function deliveryOptionsHTML(matchingProduct) {
+function deliveryOptionsHTML(matchingProduct, cartItem) {
   let html = '';
   deliveryOptions.forEach((deliveryOption) => {
     const today = dayjs();
@@ -101,10 +116,12 @@ function deliveryOptionsHTML(matchingProduct) {
         ? 'FREE'
         : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
     html += `<div class="delivery-option">
             <input
               type="radio"
-              checked
+              ${isChecked ? 'checked' : ''}
               class="delivery-option-input"
               name="delivery-option-${matchingProduct.id}"
             />

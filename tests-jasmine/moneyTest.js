4fb11acb005689ad4d71682cb711520b1cd0dcd6
MarkJudayaDev <markazjudaya@gmail.com>
2025-08-20 16:37:14 +0800

    Testing

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,9 @@
+import { formatCurrency } from '../../scripts/utils/money.js';
+
+//describe function is provided by Jasmine and it creates a test suite
+describe('test suite: formatCurrency', () => {
+  // 'it' is a function that creates a test, the first parameter (String) is the test name
+  it('convert cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+});

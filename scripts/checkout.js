074981b8c5013445c9b57040e809d1d161651a69
MarkJudayaDev <markazjudaya@gmail.com>
2026-02-12 12:09:19 +0800

    converting an object into a class

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //importing all methods in a file
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
